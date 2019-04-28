c7cc0cf75187a987b2ca8773e2b05439ea14346a
vivemeno <viveksmenon87@gmail.com>
2019-04-28 13:17:58 -0700

    Committing Phase 3 file for new function addition

---
+++ src/project/GlobalProjectValues.java
@@ -0,0 +1,17 @@
+package project;
+
+import global.NodeTable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+public class GlobalProjectValues {
+    public static final int NUMBUF = 100;
+    public static final int TAG_LENGTH = 5;
+    public static boolean OK = true;
+    public static boolean FAIL = false;
+    //	private String input_file_base = "/home/akhil/MS/DBMS/";
+    public static String input_file_base = "/home/vivemeno/DBMSI/input/";
+    public static String compositeIndexName = "compositeIndex.in";
+}
